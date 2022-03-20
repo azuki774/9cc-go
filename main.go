99946307de5b8dfddac9e3a2ad6fbe43ab5e8539
azuki774 <naruki.d@gmail.com>
2022-03-20 17:00:30 +0900

    add cobra tool

---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2022 NAME HERE <EMAIL ADDRESS>
+
+*/
+package main
+
+import "github.com/azuki774/9cc-go/cmd"
+
+func main() {
+	cmd.Execute()
+}
---
+++ makefile
@@ -0,0 +1,9 @@
+.PHONY: build
+build:
+	go build -o ./build/9cc-go .
+
+.PHONY: test
+test:
+	gofmt -l -w .
+	go test ./... -v -cover
+
