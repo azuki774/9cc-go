dc8118cb25fdc832d97d95341ae8af4667ee4648
azuki774 <azuki774s@gmail.com>
2022-04-03 02:18:23 +0900

    add nextWord in ss

--- internal/parser/stream.go
+++ internal/parser/stream.go
@@ -25,6 +25,62 @@ func (ss *stringStream) nextPeekChar() (b byte) {
 	return b
 }
 
+// 今の文字が2文字目以降の変数名、キーワードとして使えるかどうか
+func (ss *stringStream) isNextWordCharNoPrefix() bool {
+	if !ss.ok() {
+		return false
+	}
+
+	b := ss.str[ss.index]
+
+	if BYTE_a <= b && b <= BYTE_z { // a <= var b <= z
+		return true
+	}
+
+	if BYTE_A <= b && b <= BYTE_Z { // A <= var b <= Z
+		return true
+	}
+
+	if contains(b, TK_DIGIT) {
+		return true
+	}
+
+	if b == BYTE_UNDERBAR {
+		return true
+	}
+
+	return false
+}
+
+// この位置から次の変数、キーワードとして使えるWordを取り出す
+func (ss *stringStream) nextWord() (s string) {
+	if !ss.ok() {
+		return ""
+	}
+
+	// 1文字目
+	b := ss.str[ss.index]
+
+	if (BYTE_a <= b && b <= BYTE_z) || (BYTE_A <= b && b <= BYTE_Z) { // a <= var b <= z or A <= var b <= Z
+		s += string(b)
+		ss.nextChar()
+	} else {
+		// 1文字が既に無効
+		return ""
+	}
+
+	for {
+		if ss.isNextWordCharNoPrefix() {
+			// 2文字目以降が続きとして有効
+			s += string(ss.nextChar())
+		} else {
+			break
+		}
+	}
+
+	return s
+}
+
 func (ts *tokenStream) ok() bool {
 	return !(ts.tokens[ts.index].kind == TK_EOF)
 }
